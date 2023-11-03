f8b4e7d30f445988b042b51b9957e807e056bc86
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-11-03 19:50:34 -0300

    dsd

--- src/pages/DetallePage.jsx
+++ src/pages/DetallePage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Detalle from "../components/Detalle";
 import Footer from "../components/Footer";
-
+import NavbarAlt from "../components/NavbarAlt";
 export default function DetallePage(){
     return(
         <div>
-        <Navbar/>
+        <NavbarAlt/>
         <Detalle/>
         <Footer/>
         </div>
