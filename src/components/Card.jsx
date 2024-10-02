1cc3cc3551a09debf1896e3130b7e9b81c7fd6b1
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-10-02 16:20:58 -0300

    e

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,14 +5,10 @@ import "./Card.css";
 const Card = (props) => {
     return (
       <div className="project-card">
-        {/* Usar un enlace <a> con target="_blank" para abrir en una nueva pestaña */}
         <a href={props.deploy} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
           <img src={props.imgsrc} alt="imagen" />
           <h3 className="project-title">{props.title}</h3>
           <div className="pro-details">
-            <div className="pro-btns">
-              {/* Puedes agregar más botones o contenido aquí */}
-            </div>
           </div>
         </a>
       </div>
